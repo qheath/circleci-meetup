29544723101c81d747244b1c6f7836973dcb2a15
Quentin Heath <qh.dawan+doorkeeper@gmail.com>
2018-06-06 20:10:22 +0900

    bugfix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const fizzBuzz = function(n) {
   // return 'Fizz' if n is multiples of 3
   if (n % 3 === 0) {
+    // But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
+    if (n % 5 === 0) {
+      return "Fizz Buzz";
+    }
+
     return "Fizz";
   }
 
   // return 'Buzz' if n is multiples of 5
   if (n % 5 === 0) {
     return "Buzz";
-  }
-
-  // But, we want to return 'Fizz Buzz' if n is multiples of 3 and multiples of 5
-  if (n % 3 === 0 && n % 5 === 0) {
-    return "Fizz Buzz";
   }
 
   return n;
